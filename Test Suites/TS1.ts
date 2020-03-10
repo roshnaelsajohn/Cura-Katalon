f21ff8c8ef5f3b353d3869478085d4520bcf504f
roshna mittu <roshna mittu@LAPTOP-H6UUO3ES>
2020-03-10 20:32:13 +0530

    Cura first project commit

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test Suite1</description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7311d1c6-4db0-41f8-8062-3155d9ac5b09</testSuiteGuid>
+   <testCaseLink>
+      <guid>e3d2c633-053a-4897-abce-d811304a2d53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/curatestcase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TS2.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7274d34f-71d6-44a8-b8c5-f3874d256a05</testSuiteGuid>
+   <testCaseLink>
+      <guid>8108166c-2a6f-403e-98f5-26c7471adee2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/curatestcase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
